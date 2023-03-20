3f5c7f7ddb86781d02697b05e9adf4367d1ada9b
Sabri Mohiuddin <you@example.com>
2023-03-20 11:53:31 -0400

    Forms with Events Lab_sabri

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-console.log("Code your solution!")
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const error = document.querySelector('p');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const input = document.getElementById('list');
+  const text = input.value;
+
+  if (!text) {
+    error.textContent = 'Error. Todo cannot be empty';
+    return;
+  }
+
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+  input.value = '';
+});
+
+ul.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.style.textDecoration = 'line-through';
+  }
+});
+
+
+
+
+
+// const list = document.querySelector("form");
+//       list.addEventListener("submit" , (event) => {
+//       event.preventDefault();
+
+//     const unOrdered = document.querySelector("ul")
+//     const listText = document.getElementById("list").value;
+
+//     if (listText){
+//         const newLi = document.createElement("li")
+//         newLi.textContent = listText
+//         unOrdered.append(newLi)
+//     }
+// })
+
+
+// const formElement = document.getElementById('form')
+//       formElement.addEventListener("submit", (event) => {
+//       event.preventDefault();
+
+//     const listElement = document.createElement('li');
+//     const textInputValue = event.target.testInput.value;
+
+//     listElement.textContent = textInputValue;
+
+//     listElement.addEventListener('click', handleClick => {
+//     handleClick.target.style.textDecoration = "line-through";
+
+//     })
+//         const unorderedListElement = document.querySelector('ul');
+//         unorderedListElement.append(listElement);
+//         event.target.testInput.value = '';
+
+//     })
+
